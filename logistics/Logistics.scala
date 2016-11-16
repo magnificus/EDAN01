3482c8828ce6b1a2370ab05e5ff449fb3bf4f92c
AxelTLarsson <mail@axellarsson.nu>
2016-11-16 17:24:53 +0100

    Complete lab 2

--- logistics/Logistics.scala
+++ logistics/Logistics.scala
@@ -4,76 +4,87 @@ import scala.collection.mutable
 object Logistics extends App with jacop {
   val graph_size = 6
   val start = 1
-  val dest = Array(6)
-  val n_edges = 7
-  val input_from =  Array(1,1,2,2,3,4,4)
-  val input_to =    Array(2,3,3,4,5,5,6)
+  val dest = Array(5,6)
+  val n_edges = 9
+  val input_from =  Array(1,1,1,2,2,3,3,3,4)
+  val input_to =    Array(2,3,4,3,5,4,5,6,6)
   val from = input_from ++ input_to
   val to = input_to ++ input_from
-  val input_cost = Array(4,2,5,10,3,4,11)
+  val input_cost = Array(6,1,5,5,3,5,6,4,2)
   val cost = input_cost ++ input_cost
-
-  val vars: Array[IntVar] = Array.fill(graph_size)(new IntVar())
+  val edges_used = Array.fill(n_edges*2)(new IntVar("e", 0, 1))
+  //val vars: Array[IntVar] = Array.fill(graph_size)(new IntVar())
+  val vars = Array.ofDim[IntVar](dest.length, graph_size)
   val domains = Array.fill(graph_size)(new IntSet())
 
 
   // Build graph
-  from.zipWithIndex.foreach{ case (v, i) => {
-    domains(v - 1) += to(i)
-    domains(v - 1) += v
-    vars(v - 1) = new IntVar(s"x${v.toString}", domains(v - 1))
-  }}
-
-  // Build cost map
-  val map: mutable.Map[(Int, Int), Int] = mutable.Map()
-
-  from.zipWithIndex.foreach{ case (v, i) => {
-    map += (v, to(i)) -> cost(i)
-  }}
-
-  (1 to graph_size).foreach(v => {
-    map += (v, v) -> 0
-  })
+    from.zipWithIndex.foreach{ case (v, i) => {
+      domains(v - 1) += to(i)
+      domains(v - 1) += v
+      (0 to dest.length - 1).foreach(d => {
+        vars(d)(v - 1) = new IntVar(s"x${v.toString}", domains(v - 1))
+      })
+    }}
 
   // Add start to domain of dests
-  dest.foreach{ d =>
-    domains(d - 1) += start
-    vars(d - 1) = new IntVar(s"x${d.toString}", domains(d - 1))
-    map += (d, start) -> 0
+  (0 to dest.length - 1).foreach(i => {  
+    dest.foreach{ d =>
+      val domainCopy = domains.clone
+      domainCopy(d - 1) += start
+      vars(i)(d - 1) = new IntVar(s"x${d.toString}", domainCopy(d - 1))
+    }
+  })
+
+  (0 to dest.length - 1).foreach(i => {
+    subcircuit(vars(i))
+  })
+
+
+  (0 to dest.length - 1).foreach(j => {
+    edges_used.zipWithIndex.foreach{ case (v, i) => {
+      val edge = new IntVar()
+      edge #= abs(vars(j)(from(i)-1) - to(i))
+      val temp = new IntVar("t")
+
+      //edge #= abs(edge)
+      val const = new IntVar()
+      const #= 1
+      val a = Array(edge, const)
+      edges_used(i) >= abs(min(a)-1)
+    }}
+  })
+
+  println(cost.mkString(" "))
+  val reward = new IntVar("r", 0, 1000)
+  //reward #= sumDom(edges_used, cost)
+  
+  val totCosts = edges_used.zip(cost).map{ (a) =>
+   a._1 * a._2
   }
+  reward #= sum[IntVar](totCosts)
 
+  // Dest must be in the solution of corresponding vars
+  (0 to dest.length -1).foreach(d => {
+    vars(d)(dest(d) - 1) #\= dest(d)
+    // Start must be in the solution
+    vars(d)(start - 1) #\= start
+  })
 
-  println(vars.mkString(" "))
-  subcircuit(vars)
+  val all_arrays = vars(0) ++ vars(1)
 
-  // Dest must be in the solution
-  dest.foreach(d =>
-    vars(d - 1) #\= d
-  )
-
-  // Start must be in the solution
-  vars(start - 1) #\= start
-    // totalCost
-
-  val min = new IntVar("min-cost", -10000, 10000)
-  val intVector = Array.fill(graph_size)(new IntVar())
-  vars.zipWithIndex.foreach{ case (v, i) => {
-    val theCost = (map get (i + 1, v.value())).get
-    intVector(i) #= theCost
-  }}
-  println(intVector.mkString(" "))
-  //min #= sum(vars, intVector)
-
-  val result = satisfyAll(search(vars, input_order, indomain_min), printSol)
-
-  statistics
+  val result = minimize(search(all_arrays, input_order, indomain_min), reward)
+  //statistics
 
   def printSol(): Unit = {
     print("Solution: ")
       for (v <- vars)
         print(v + " ")
+      println("\nedges_used: ")
+      for (e <- edges_used)
+        print(e + " ")
       println
-    println("min: " + min.value())
+    println("reward: " + reward.dom)
   }
 
 }
